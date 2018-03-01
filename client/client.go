001bfae44879b479bb54ec3b9c8afd618820cf88
Bsg Itis <itisbsg@gmail.com>
2018-02-28 21:44:21 -0800

    Initial commit

---
+++ client/client.go
@@ -0,0 +1,140 @@
+//
+// client.go
+//
+// gRPC Push Notification Client
+//
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"math/rand"
+	"time"
+
+	pb "grpc-push-notif/protos"
+
+	"google.golang.org/grpc"
+)
+
+var name string
+var subChan chan pb.TopicType
+
+const (
+	address = "localhost:50051"
+)
+
+// register ...
+func register(client pb.PushNotifClient) error {
+	log.Printf("Calling Register RPC")
+
+	resp, err := client.Register(context.Background(), &pb.RegistrationRequest{ClientName: name})
+	if err != nil {
+		log.Fatalf("Register failed %v", err)
+	}
+	log.Print(resp, err)
+	// cookie = resp.ClientCookie
+	// log.Printf("Reg Response %s, %s, %v", resp.ClientName, resp.ServerName, resp.ClientCookie)
+
+	return nil
+}
+
+// subscribeToAlerts ...
+func subscribeToAlerts(client pb.PushNotifClient) {
+	log.Println("Subscribing to Alerts")
+
+	stream, err := client.Alert(context.Background(), &pb.Topic{ClientName: name, Type: pb.TopicType_ALERT})
+	if err != nil {
+		log.Fatalf("failed to subscribe to Alerts %v", err)
+	}
+
+	for {
+		resp, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("failed to recv alerts")
+		}
+		log.Print(resp, err)
+	}
+}
+
+// subscribeToModeChanges ...
+func subscribeToModeChanges(c chan pb.TopicType) {
+	log.Println("Subscribing to Mode changes")
+
+	c <- pb.TopicType_MODE
+
+	fmt.Println("done queuing it to channel c")
+}
+
+// subscribeToModeChanges ...
+func subscribeToTempChanges(c chan pb.TopicType) {
+	log.Println("Subscribing to Temp changes")
+
+	c <- pb.TopicType_TEMP
+
+}
+
+// recvNotification ...
+func recvNotification(stream pb.PushNotif_SubscribeClient) {
+	for {
+		resp, err := stream.Recv()
+		if err != nil {
+			log.Fatalf("failed to recv %v", err)
+		}
+		if err == io.EOF {
+			break
+		}
+		log.Print(resp, err)
+	}
+}
+
+// subscribe ...
+func subscribe(client pb.PushNotifClient, c chan pb.TopicType) {
+	stream, err := client.Subscribe(context.Background())
+	if err != nil {
+		log.Fatalf("failed to subscribe %v", err)
+	}
+
+	go recvNotification(stream)
+
+	for t := range c {
+		log.Printf("sending subscibe for topic %s", t.String())
+		if err := stream.Send(&pb.Topic{ClientName: name, Type: t}); err != nil {
+			log.Fatalf("couldnt send %s", t.String())
+		}
+	}
+	stream.CloseSend()
+}
+
+// main ...
+func main() {
+
+	// init important structures
+	subChan = make(chan pb.TopicType, 10)
+	rand.Seed(time.Now().UTC().UnixNano())
+	name = fmt.Sprintf("%s:%d", "Client", rand.Intn(50))
+
+	// Setup a connection with the server
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	client := pb.NewPushNotifClient(conn)
+
+	register(client)
+
+	// Subscribe to alerts.. a way to request for push notifications.
+	// go routine below keeps the stream long lived and listens to
+	// alerts as long as the connection is up
+	go subscribeToAlerts(client)
+
+	subscribeToModeChanges(subChan)
+	subscribeToTempChanges(subChan)
+	subscribe(client, subChan)
+}
---
+++ server/server.go
@@ -0,0 +1,126 @@
+//
+// server.go
+//
+// gRPC Push Notification Server
+//
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+	"time"
+
+	pb "grpc-push-notif/protos"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type server struct {
+	name      string
+	alertStrm map[string]pb.PushNotif_AlertServer
+	subStrm   map[string]pb.PushNotif_SubscribeServer
+}
+
+func newServer() *server {
+	return &server{name: "pushnotifServer", alertStrm: make(map[string]pb.PushNotif_AlertServer),
+		subStrm: make(map[string]pb.PushNotif_SubscribeServer)}
+}
+
+func (s *server) Register(ctx context.Context, req *pb.RegistrationRequest) (*pb.RegistrationResponse, error) {
+	log.Printf("Received a Registration Request from %s", req.GetClientName())
+	return &pb.RegistrationResponse{ClientName: req.GetClientName(), ServerName: s.name}, nil
+}
+
+func (s *server) Alert(topic *pb.Topic, stream pb.PushNotif_AlertServer) error {
+	log.Printf("Received a Alert request for topic %s from client %s", topic.GetType().String(), topic.GetClientName())
+	s.alertStrm[topic.GetClientName()] = stream
+	log.Printf("Client %s will now recieve Alerts", topic.GetClientName())
+
+	// long lived stream
+	for {
+	}
+
+}
+
+func (s *server) Subscribe(stream pb.PushNotif_SubscribeServer) error {
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		log.Printf("Received a subscribe request for topic %s from client %v", in.GetType().String(), in.GetClientName())
+		_, ok := s.subStrm[in.GetClientName()+":"+in.GetType().String()]
+		if !ok {
+			s.subStrm[in.GetClientName()+":"+in.GetType().String()] = stream
+			// log.Printf("Client %v will now recieve updates about topic %s", in.GetClientName(), in.GetType().String())
+		} else {
+			log.Printf("Client %s already subscribed to %s", in.GetClientName(), in.GetType().String())
+		}
+	}
+	return nil
+}
+
+func (s *server) pushUpdates() {
+	var str string
+	var i uint32
+
+	time.Sleep(10 * time.Second)
+
+	for {
+		fmt.Println(s.alertStrm)
+		for k, v := range s.alertStrm {
+			fmt.Printf("Enter new Alert for Client(%v): ", k)
+			fmt.Scan(&str)
+			if err := v.Send(&pb.Notification{Type: pb.TopicType_ALERT, A: &pb.Alert{Message: str}}); err != nil {
+				log.Fatalf("Send failed %v", err)
+			}
+		}
+
+		//fmt.Println("walk thru the subscribe streams")
+		for k, v := range s.subStrm {
+			if strings.Contains(k, "MODE") {
+				fmt.Printf("Enter new Mode for Client(%v): ", k)
+				fmt.Scan(&str)
+				if err := v.Send(&pb.Notification{Type: pb.TopicType_MODE, M: &pb.Mode{NewMode: str}}); err != nil {
+					log.Fatalf("Send failed %v", err)
+				}
+			} else {
+				fmt.Printf("Enter new Temp for Client(%v): ", k)
+				fmt.Scan(&i)
+				if err := v.Send(&pb.Notification{Type: pb.TopicType_TEMP, T: &pb.Temp{NewTemp: i}}); err != nil {
+					log.Fatalf("Send failed %v", err)
+				}
+			}
+		}
+	}
+}
+
+// main ...
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	myServer := newServer()
+	pb.RegisterPushNotifServer(s, myServer)
+
+	go myServer.pushUpdates()
+
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
